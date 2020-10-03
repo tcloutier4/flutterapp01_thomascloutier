b257c357295043f56dc1dd7d0dcd3495a40a403e
Thomas Cloutier <thomas.cloutier.r@gmail.com>
2020-10-02 21:25:58 -0600

    Container builder

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,20 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class ContainerName extends StatelessWidget {
+  final String name;
+  final Color color;
+
+  const ContainerName(this.name, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(10),
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(color: color),
+        child: Text(name, textAlign: TextAlign.center));
+  }
+}
