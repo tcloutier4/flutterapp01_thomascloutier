b86435895253c34112188ec224e6e95e7fc13f94
Thomas Cloutier <thomas.cloutier.r@gmail.com>
2020-10-03 12:16:30 -0600

    Final commit, comment above container 2

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,43 @@ class MyApp extends StatelessWidget {
           children: [
             Column(
               children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.orange, border: Border.all(width: 3)),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.orange, border: Border.all(width: 3)),
+                      child: Center(child: Text('Container 1'))),
                 ),
-                Container(
-                    transform: Matrix4.rotationZ(pi / 4),
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(color: Colors.white))
+                /*Couldn't figure out how to make the container 2 move properly
+                tried putting it in a stack and using positioned, but it 
+                overflowed by infinity pixels on the right the following was my
+                thought process/code, with probably different values for 
+                top and left to position it properly*/
+/*
+                Stack(
+                  children: [
+                    Positioned(
+                        top: 100,
+                        left: 10,
+                        child: Container(
+                            transform: Matrix4.rotationZ(pi / 4),
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(color: Colors.white),
+                            child: Center(child: Text('Container 2'))))
+                  ],
+                ),
+*/
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Container(
+                        transform: Matrix4.rotationZ(pi / 4),
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(color: Colors.white),
+                        child: Center(child: Text('Container 2'))))
               ],
             ),
             Column(
@@ -45,22 +70,28 @@ class MyApp extends StatelessWidget {
                         child: Container(
                             height: 100,
                             width: 100,
-                            decoration: BoxDecoration(color: Colors.yellow)))),
+                            decoration: BoxDecoration(color: Colors.yellow),
+                            child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text('Container 3'))))),
                 Expanded(
                     child: Padding(
                         padding: const EdgeInsets.all(10),
                         child: Container(
-                            padding: EdgeInsets.all(10),
                             height: 100,
                             width: 100,
-                            decoration: BoxDecoration(color: Colors.blue))))
+                            decoration: BoxDecoration(color: Colors.blue),
+                            child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Text('Container 4')))))
               ],
             ),
             Column(
               children: [
                 Expanded(
+                    flex: 2,
                     child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 70, 10, 70),
+                        padding: const EdgeInsets.all(10),
                         child: Container(
                             height: 100,
                             width: 100,
@@ -68,15 +99,22 @@ class MyApp extends StatelessWidget {
                               shape: BoxShape.circle,
                               color: Colors.black,
                               border: Border.all(width: 3, color: Colors.white),
-                            )))),
-                // Expanded(child:
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(color: Colors.red),
-                    ))
+                            ),
+                            child: Center(
+                                child: Text('Container 5',
+                                    style: TextStyle(color: Colors.white)))))),
+                Expanded(
+                    flex: 1,
+                    child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(color: Colors.red),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text('Con 6',
+                                    style: TextStyle(fontSize: 30))))))
               ],
             ),
           ],
